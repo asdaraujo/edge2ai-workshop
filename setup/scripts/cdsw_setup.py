e834fcf043abc92baf3940f6ad8fbce2c88c134d
Andre Araujo <araujo@cloudera.com>
2019-08-20 19:21:39 -0700

    General updates + CDSW setup script

---
+++ setup/scripts/cdsw_setup.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import time
+
+PUBLIC_IP = sys.argv[1]
+CDSW_API = 'http://cdsw.%s.nip.io/api/v1' % (PUBLIC_IP,)
+CDSW_ALTUS_API = 'http://cdsw.%s.nip.io/api/altus-ds-1' % (PUBLIC_IP,)
+
+USERNAME = 'admin'
+PASSWORD = 'supersecret1'
+FULL_NAME = 'Workshop Admin'
+EMAIL = 'admin@cloudera.com'
+
+print('# Prepare CDSW for workshop')
+try:
+    s = requests.Session()
+    
+    print('# Create user')
+    while True:
+        r = s.post(CDSW_API + '/users', json={
+                'email': EMAIL,
+                'name': FULL_NAME,
+                'username': USERNAME,
+                'password': PASSWORD,
+                'type': 'user'
+            })
+        if r.status_code == 201:
+            break
+        elif r.status_code == 422:
+            raise RuntimeError('User admin already exists')
+        print('Waiting for CDSW to be ready... (error code: %s)' % (r.status_code,))
+        time.sleep(10)
+    exit()
+
+    print('# Authenticate')
+    r = s.post(CDSW_API + '/authenticate', json={'login': USERNAME, 'password': PASSWORD})
+    s.headers.update({'Authorization': 'Bearer ' + r.json()['auth_token']})
+    
+    print('# Add engine')
+    r = s.post(CDSW_API + '/site/engine-profiles', json={'cpu': 2, 'memory': 4})
+    engine_id = r.json()['id']
+    print('Engine ID: %s'% (engine_id,))
+    
+    print('# Add environment variable')
+    r = s.patch(CDSW_API + '/site/config', json={'environment': '{"HADOOP_CONF_DIR":"/etc/hadoop/conf/"}'})
+    
+    print('# Add project')
+    r = s.post(CDSW_API + '/users/admin/projects', json={'template': 'git',
+                                                         'project_visibility': 'private',
+                                                         'name': 'Edge2AI Workshop',
+                                                         'gitUrl': 'https://github.com/asdaraujo/edge2ai-workshop'})
+    project_id = r.json()['id']
+    print('Project ID: %s'% (project_id,))
+    
+    print('# Upload setup script')
+    setup_script = """
+    !pip3 install --upgrade pip scikit-learn
+    !hdfs dfs -put data/historical_iot.txt /user/$HADOOP_USER_NAME
+    """
+    r = s.put(CDSW_API + '/projects/admin/edge2ai-workshop/files/setup_workshop.py', files={'name': setup_script})
+    
+    print('# Create job to run the setup script')
+    r = s.post(CDSW_API + '/projects/admin/edge2ai-workshop/jobs', json={
+            'name': 'Setup workshop',
+            'type': 'manual',
+            'script': 'setup_workshop.py',
+            'timezone': 'America/Los_Angeles',
+            'environment':{},
+            'kernel': 'python3',
+            'cpu': 2,
+            'memory': 4,
+            'nvidia_gpu': 0,
+            'notifications': [{
+                'user_id': 1,
+                'success': False,
+                'failure': False,
+                'timeout': False,
+                'stopped': False
+            }],
+            'recipients': {},
+            'attachments': [],
+        })
+    job_id = r.json()['id']
+    print('Job ID: %s' % (job_id,))
+    
+    print('# Start job')
+    job_url = '%s/projects/admin/edge2ai-workshop/jobs/%s' % (CDSW_API, job_id)
+    start_url = '%s/start' % (job_url,)
+    
+    r = s.post(start_url, json={})
+    
+    while True:
+        r = s.get(job_url)
+        status = r.json()['latest']['status']
+        print('Job %s status: %s' % (job_id, status))
+        if status == 'succeeded':
+            break
+        time.sleep(10)
+    
+    print('# Run experiment')
+    r = s.post(CDSW_ALTUS_API + '/ds/run', json={
+            'script': 'cdsw.iot_exp.py',
+            'arguments': '20 20',
+            'kernel': 'python3',
+            'cpu': 2,
+            'memory': 4,
+            'project': str(project_id),
+        })
+    run_id = r.json()['id']
+    print('Experiment run ID: %s' % (run_id,))
+    
+    while True:
+        r = s.post(CDSW_ALTUS_API + '/ds/listruns', json={
+            'project': str(project_id),
+            'pageSize': 30,
+            'startingToken': 0,
+            'orderBy': 'createdAt',
+            'orderSort': 'desc',
+            'metricsOrder': ''})
+        runs = [r for r in r.json()['runs'] if r['id'] == run_id]
+        if runs:
+            status = runs[0]['status']
+            print('Experiment run %s status: %s' % (run_id, status))
+            if status == 'succeeded':
+                break
+        time.sleep(10)
+    
+    print('# Promote output')
+    r = s.post(CDSW_ALTUS_API + '/ds/promoteRunOutput', json={'id': str(run_id), 'files': ["iot_model.pkl"]})
+    
+    print('# Get engine image to use for model')
+    r = s.get(CDSW_API + '/projects/admin/edge2ai-workshop/engine-images')
+    engine_image_id = r.json()['id']
+    print('Engine image ID: %s' % (engine_image_id,))
+    
+    print('# Deploy model')
+    r = s.post(CDSW_ALTUS_API + '/models/create-model', json={
+            'projectId': project_id,
+            'name': 'IoT Prediction Model',
+            'description': 'IoT Prediction Model',
+            'visibility': 'private',
+            'targetFilePath': 'cdsw.iot_model.py',
+            'targetFunctionName': 'predict',
+            'engineImageId': engine_image_id,
+            'kernel': 'python3',
+            'examples': [{'request': {'feature': '0, 65, 0, 137, 21.95, 83, 19.42, 111, 9.4, 6, 3.43, 4'}}],
+            'cpuMillicores': 2000,
+            'memoryMb': 4096,
+            'replicationPolicy': {'type': 'fixed', 'numReplicas': 1},
+            'environment': {},
+        })
+    model_id = r.json()['id']
+    print('Model ID: %s' % (model_id,))
+    
+    while True:
+        r = s.post(CDSW_ALTUS_API + '/models/list-models', json={
+            'project': str(project_id),
+            'latestModelDeployment': True,
+            'latestModelBuild': True,
+        })
+        models = [m for m in r.json() if m['id'] == str(model_id)]
+        if models:
+            build_status = models[0]['latestModelBuild']['status']
+            deployment_status = models[0]['latestModelDeployment']['status']
+            print('Model %s: build status: %s, deployment status: %s' % (model_id, build_status, deployment_status))
+            if build_status == 'built' and deployment_status == 'deployed':
+                break
+        time.sleep(10)
+except Exception as e:
+    print(r.text)
+    raise
--- setup/terraform/browse-cluster-socks.sh
+++ setup/terraform/browse-cluster-socks.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Works on MacOS only
-
 set -u
 set -e
 BASE_DIR=$(cd $(dirname $0); pwd -L)
@@ -12,7 +10,13 @@ if [ ! -f .key.file.name -o ! -f .instance.list ]; then
 fi
 PUBLIC_DNS=$(awk '$1 ~ /-'$CLUSTER_ID'$/{print $2}' .instance.list)
 PUBLIC_IP=$(awk '$1 ~ /-'$CLUSTER_ID'$/{print $3}' .instance.list)
+PRIVATE_DNS=$(awk '$1 ~ /-'$CLUSTER_ID'$/{print $4}' .instance.list)
 KEY_FILE=$BASE_DIR/$(cat $BASE_DIR/.key.file.name)
+
+ssh -o StrictHostKeyChecking=no -i $KEY_FILE -CND $PROXY_PORT centos@$PUBLIC_DNS &
+CHILD_PID=$!
+
+trap "kill $CHILD_PID" 0
 
 rm -rf $HOME/chrome-for-demo
 mkdir $HOME/chrome-for-demo
@@ -20,12 +24,14 @@ touch "$HOME/chrome-for-demo/First Run"
 
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
   --user-data-dir="$HOME/chrome-for-demo" \
-  http://$PUBLIC_DNS:7180 \
-  http://$PUBLIC_DNS:10080/efm/ui \
-  http://$PUBLIC_DNS:8080/nifi/ \
-  http://$PUBLIC_DNS:18080/nifi-registry \
-  http://$PUBLIC_DNS:7788 \
-  http://$PUBLIC_DNS:9991 \
-  http://$PUBLIC_DNS:8888 \
+  --proxy-server="socks5://localhost:8157" \
+  --window-size=1184,854 \
+  http://$PRIVATE_DNS:7180 \
+  http://$PRIVATE_DNS:10080/efm/ui \
+  http://$PRIVATE_DNS:8080/nifi/ \
+  http://$PRIVATE_DNS:18080/nifi-registry \
+  http://$PRIVATE_DNS:7788 \
+  http://$PRIVATE_DNS:9991 \
+  http://$PRIVATE_DNS:8888 \
   http://cdsw.$PUBLIC_IP.nip.io
 
