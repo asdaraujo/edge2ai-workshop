d585b1847ab8ae61429c8ea97147eaf864237f2b
Andre Araujo <araujo@cloudera.com>
2019-09-22 16:26:00 -0700

    Fix logging for run-on-all-clusters command

--- setup/terraform/run-on-all-clusters.sh
+++ setup/terraform/run-on-all-clusters.sh
@@ -18,10 +18,13 @@ KEY_FILE=$BASE_DIR/$(cat $BASE_DIR/.key.file.name)
 
 cmd=("$@")
 for line in $(awk '{print $1":"$3}' .instance.list); do
-  cluster_name=$(echo "$line" | awk -F: '{print $1}')
+  cluster_name="$(echo "$line" | awk -F: '{print $1}'): "
   public_ip=$(echo "$line" | awk -F: '{print $2}')
-  cluster_name="$(echo -e "\033[0m\033[1m${cluster_name}:\033[0m") "
-  ssh -q -o StrictHostKeyChecking=no -i $KEY_FILE centos@$public_ip "${cmd[@]}" 2>&1 | sed "s/^/${cluster_name}/"
+  #cluster_name="$(echo -e "\033[0m\033[1m${cluster_name}:\033[0m") "
+  bold="$(echo -e "\033[0m\033[1m")"
+  normal="$(echo -e "\033[0m")"
+  ssh -q -o StrictHostKeyChecking=no -i $KEY_FILE centos@$public_ip "${cmd[@]}" 2>&1 | \
+    sed "s/^/${cluster_name}/" | tee $LOG_FILE | sed "s/^[^:]*:/${bold}&${normal}/"
 done
 
 wait
