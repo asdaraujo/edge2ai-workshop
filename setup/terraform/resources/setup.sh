26e375d35ad75a21ead180681d79f30df3723b00
Andre Araujo <araujo@cloudera.com>
2020-01-15 11:38:57 -0800

    Fixed NAR download from Maven repo
    Since yesterday it seems that central.maven.org stopped accepting
    plain HTTP requests and now requests must go to repo1.maven.org
    and use HTTPS.

--- setup/terraform/resources/setup.sh
+++ setup/terraform/resources/setup.sh
@@ -101,7 +101,7 @@ if [[ ! -f $CM_REPO_FILE ]]; then
   systemctl disable minifi
 
   echo "-- Download and install MQTT Processor NAR file"
-  wget http://central.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
+  wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-mqtt-nar/1.8.0/nifi-mqtt-nar-1.8.0.nar -P /opt/cloudera/cem/minifi/lib
   chown root:root /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
   chmod 660 /opt/cloudera/cem/minifi/lib/nifi-mqtt-nar-1.8.0.nar
 
