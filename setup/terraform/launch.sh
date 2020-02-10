be3ed844cc5548765abbc9d23560d7e02c3bcdf3
Daniel Chaffelson <chaffelson@gmail.com>
2020-02-10 08:35:18 +0000

    Epic: Fix Packer and upgrade Py3 as dependency Significant restructuring of setup.sh to reduce deployment time to under 30mins Replace using system python2 with installed python3 to avoid breaking on reboot during Packer build Updated labs and images to py3 Fixed Packer to work with new builds Rename default Packer ami_name to 'cdfworkshop' to make it easier to find Added Packer test request to Contributing doc Reworked pytest Tests fpr py3 Set default execution for most python code to python3 Impala-shell lacks py3 compatibility, switched to native impyla for cleaner interfacing Added control SKIP_CDSW to skip cdsw tests if cdsw not built during deployment Added instructions to Contributing doc for pytest tests Added new TF_VAR_parallelism to .env.template to limit parallel deployment width Updated DockerFile to include Packer and Terraform by default, otherwise behaves the same
    Other:
    Added null provider minimum version to Terraform to remove warning message
    Updated template to HTTPS endpoints for Cloudera.com and Maven.

--- setup/terraform/launch.sh
+++ setup/terraform/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASE_DIR=$(cd $(dirname $0); pwd -L)
-source $BASE_DIR/common.sh
+source ${BASE_DIR}/common.sh
 
-mkdir -p $BASE_DIR/logs
+mkdir -p "${BASE_DIR}"/logs
 (
 set -o errexit
 set -o nounset
@@ -27,21 +27,21 @@ dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
 print((datetime.strptime('$TF_VAR_enddate', '%m%d%Y') - dt).days)
 ")
 
-if [ "$DATE_CHECK" -le "0" ]; then
+if [ "${DATE_CHECK}" -le "0" ]; then
   echo 'ERROR: The expiration date for your environment is either set for today or already in the past.'
   echo '       Please update "TF_VAR_endddate" in .env.'"$NAMESPACE"' and try again.'
   exit 1
 elif [ "$DATE_CHECK" -le "$WARNING_THRESHOLD_DAYS" ]; then
   echo -n "WARNING: Your environment will expire in less than $WARNING_THRESHOLD_DAYS days. Do you really want to continue? "
   read CONFIRM
-  CONFIRM=$(echo $CONFIRM | tr a-z A-Z)
+  CONFIRM=$(echo "${CONFIRM}" | tr a-z A-Z)
   if [ "$CONFIRM" != "Y" -a "$CONFIRM" != "YES" ]; then
     echo 'Please update "TF_VAR_endddate" in .env.'"$NAMESPACE"' and try again.'
     exit 1
   fi
 fi
 
-mkdir -p $NAMESPACE_DIR
+mkdir -p "${NAMESPACE_DIR}"
 
 # Perform a quick configuration sanity check before calling Terraform
 source $BASE_DIR/resources/common.sh
@@ -59,19 +59,19 @@ ensure_key_pairs
 
 log "Launching Terraform"
 terraform init
-terraform apply -auto-approve -parallelism=20 -state=$NAMESPACE_DIR/terraform.state
+terraform apply -auto-approve -parallelism="${TF_VAR_parallelism}" -state=$NAMESPACE_DIR/terraform.state
 
 log "Deployment completed successfully"
 
 echo ""
 echo "Instances:"
-$BASE_DIR/list-details.sh $NAMESPACE
+"${BASE_DIR}/list-details.sh" "${NAMESPACE}"
 
 echo ""
 echo "Health checks:"
-$BASE_DIR/check-services.sh $NAMESPACE
+"${BASE_DIR}/check-services.sh" "${NAMESPACE}"
 
-if [ "$TF_VAR_deploy_cdsw_model" == "true" ]; then
+if [ "${TF_VAR_deploy_cdsw_model}" == "true" ]; then
   echo -e "\033[33m" # set font color to yellow
   echo "    NOTE: CDSW model is being deployed in the background."
   echo "          Execute the following command later to check on the status of"
@@ -83,6 +83,6 @@ fi
 
 echo ""
 echo "Uploading instance details to Web Server:"
-$BASE_DIR/upload-instance-details.sh $NAMESPACE
+"${BASE_DIR}/upload-instance-details.sh" "${NAMESPACE}"
 
 ) 2>&1 | tee $BASE_DIR/logs/setup.log.${1:-unknown}.$(date +%Y%m%d%H%M%S)
