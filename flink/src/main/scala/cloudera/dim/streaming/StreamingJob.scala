a531a9eefc1e6b04232e197fcf6d05c56ebcf13f
vvagias <vvagias@cloudera.com>
2020-02-20 15:46:04 -0600

    fixed flink job deps

--- flink/src/main/scala/cloudera/dim/streaming/StreamingJob.scala
+++ flink/src/main/scala/cloudera/dim/streaming/StreamingJob.scala
@@ -1,4 +1,22 @@
-package vv
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package clodera.dim.streaming
 
 import java.util.{Properties, StringTokenizer}
 import org.apache.flink.api.common.functions.{FlatMapFunction, MapFunction}
@@ -8,10 +26,19 @@ import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaProducer, FlinkKafkaProducer011}
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
 
-
-
-object StreamingJobIoT {
-
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+object StreamingJob {
   def main(args: Array[String]) {
     // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
