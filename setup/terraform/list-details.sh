ecc84435cfc588f64d59e6ddf905099acc2a0bda
Andre Araujo <araujo@cloudera.com>
2020-06-18 16:59:37 -0700

    Improve performance of list-details.sh

--- setup/terraform/list-details.sh
+++ setup/terraform/list-details.sh
@@ -11,21 +11,26 @@ if [ $# -gt 1 ]; then
 fi
 NAMESPACE=${1:-}
 
+TF_JSON_DIR=/tmp/.tf.json.$$
+
 function web_instance() {
-  if [ -s $TF_JSON_FILE ]; then
-    cat $TF_JSON_FILE | jq -r '.values[]?.resources[]? | select(.address == "aws_instance.web") | "\(.values.tags.Name) \(.values.public_dns) \(.values.public_ip) \(.values.private_ip)"'
+  local tf_json_file=$1
+  if [ -s $tf_json_file ]; then
+    cat $tf_json_file | jq -r '.values[]?.resources[]? | select(.address == "aws_instance.web") | "\(.values.tags.Name) \(.values.public_dns) \(.values.public_ip) \(.values.private_ip)"'
   fi
 }
 
 function cluster_instances() {
-  if [ -s $TF_JSON_FILE ]; then
-    cat $TF_JSON_FILE | jq -r '.values[]?.resources[]? | select(.address == "aws_instance.cluster") | "\(.values.tags.Name) \(.values.public_dns) \(.values.public_ip) \(.values.private_ip)"'
+  local tf_json_file=$1
+  if [ -s $tf_json_file ]; then
+    cat $tf_json_file | jq -r '.values[]?.resources[]? | select(.address == "aws_instance.cluster") | "\(.values.tags.Name) \(.values.public_dns) \(.values.public_ip) \(.values.private_ip)"'
   fi
 }
 
 function enddate() {
-  if [ -s $TF_JSON_FILE ]; then
-    cat $TF_JSON_FILE | jq -r '.values.root_module.resources[0].values.tags.enddate' | sed 's/null//'
+  local tf_json_file=$1
+  if [ -s $tf_json_file ]; then
+    cat $tf_json_file | jq -r '.values.root_module.resources[0].values.tags.enddate' | sed 's/null//'
   fi
 }
 
@@ -35,30 +40,29 @@ function show_details() {
 
   load_env $namespace
 
-  TF_JSON_FILE=$BASE_DIR/.tf.json.$$
-  trap "rm -f $TF_JSON_FILE" 0
+  local tf_json_file=$TF_JSON_DIR/${namespace}
 
-  rm -f $TF_JSON_FILE
+  rm -f $tf_json_file
   mkdir -p $NAMESPACE_DIR
   set +e
-  (cd $BASE_DIR && terraform show -json $NAMESPACE_DIR/terraform.state > $TF_JSON_FILE 2>/dev/null)
+  (cd $BASE_DIR && terraform show -json $NAMESPACE_DIR/terraform.state > $tf_json_file 2>/dev/null)
   set +e
 
-  web_instance | while read name public_dns public_ip private_ip; do
+  web_instance "$tf_json_file" | while read name public_dns public_ip private_ip; do
     printf "%-40s %-55s %-15s %-15s\n" "$name" "$public_dns" "$public_ip" "$private_ip"
   done | sed 's/\([^ ]*-\)\([0-9]*\)\( .*\)/\1\2\3 \2/' | sort -k4n | sed 's/ [0-9]*$//' > $WEB_INSTANCE_LIST_FILE
 
-  cluster_instances | while read name public_dns public_ip private_ip; do
+  cluster_instances "$tf_json_file" | while read name public_dns public_ip private_ip; do
     printf "%-40s %-55s %-15s %-15s\n" "$name" "$public_dns" "$public_ip" "$private_ip"
   done | sed 's/\([^ ]*-\)\([0-9]*\)\( .*\)/\1\2\3 \2/' | sort -k4n | sed 's/ [0-9]*$//' > $INSTANCE_LIST_FILE
 
   if [ -s $WEB_INSTANCE_LIST_FILE ]; then
-    web_server="http://$(web_instance | awk '{print $3}')"
+    web_server="http://$(web_instance "$tf_json_file" | awk '{print $3}')"
   else
     web_server="-"
   fi
 
-  local enddate=$(enddate)
+  local enddate=$(enddate "$tf_json_file")
   local remaining_days=""
   local warning=""
   if [ "$enddate" != "" ]; then
@@ -114,16 +118,26 @@ function show_details() {
     fi
 
     if [ "${DEBUG_DETAILS:-}" != "" ]; then
-      jq -r '.' $TF_JSON_FILE
+      jq -r '.' $tf_json_file
     fi
   fi
 }
 
+rm -rf $TF_JSON_DIR
+mkdir -p $TF_JSON_DIR
+trap "rm -rf $TF_JSON_DIR" 0
+
 if [ "$NAMESPACE" == "" ]; then
   printf "%-25s %-40s %10s  %8s  %9s\n" "Namespace" "Web Server" "# of VMs" "End Date" "Days Left"
+  tmp_dir=/tmp/list-details.$$
+  rm -rf $tmp_dir
+  mkdir $tmp_dir
   for namespace in $(get_namespaces); do
-    show_details $namespace yes
+    show_details $namespace yes > $tmp_dir/$namespace &
   done
+  wait
+  cat $tmp_dir/*
+  rm -rf $tmp_dir
   echo ""
   echo "${C_YELLOW}    To list the full details for a particular namespace, use:"
   echo ""
