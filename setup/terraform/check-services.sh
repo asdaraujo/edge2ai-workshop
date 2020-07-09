8d1769e6b43557c07900d70f82b44484e9e04342
Andre Araujo <araujo@cloudera.com>
2020-07-08 20:18:40 -0700

    Make check-services.sh more robust

--- setup/terraform/check-services.sh
+++ setup/terraform/check-services.sh
@@ -3,7 +3,9 @@ set -o nounset
 BASE_DIR=$(cd $(dirname $0); pwd -L)
 source $BASE_DIR/common.sh
 
-trap "rm -f .cml .cem .cj .cm .hue .model .nifi .nifireg .schreg .smm .web" 0
+CURL=(curl -L -k --connect-timeout 5)
+
+trap "rm -f .cml.$$ .cem.$$ .cj.$$ .cm.$$ .hue.$$ .model.$$ .nifi.$$ .nifireg.$$ .schreg.$$ .smm.$$ .web.$$" 0
 
 if [ $# != 1 ]; then
   echo "Syntax: $0 <namespace>"
@@ -19,17 +21,17 @@ jq -r '.values.root_module.resources[] | select(.address == "aws_instance.web" o
 while read instance ip host; do
   CDSW_API="http://cdsw.$ip.nip.io/api/v1"
   CDSW_ALTUS_API="http://cdsw.$ip.nip.io/api/altus-ds-1"
-  (curl -L http://$host/api/ping 2>/dev/null | grep 'Pong!' > /dev/null 2>&1 && echo Ok) > .web &
-  (curl -k -L http://$host:7180/cmf/login 2>/dev/null | grep "<title>Cloudera Manager</title>" > /dev/null 2>&1 && echo Ok) > .cm &
-  ((curl -L http://$host:10080/efm/ui/        || curl -k -L https://$host:10080/efm/ui/)        2>/dev/null | grep "<title>CEM</title>" > /dev/null 2>&1 && echo Ok) > .cem &
-  ((curl -L http://$host:8080/nifi/           || curl -k -L https://$host:8443/nifi/)           2>/dev/null | grep "<title>NiFi</title>" > /dev/null 2>&1 && echo Ok) > .nifi &
-  ((curl -L http://$host:18080/nifi-registry/ || curl -k -L https://$host:18433/nifi-registry/) 2>/dev/null | grep "<title>NiFi Registry</title>" > /dev/null 2>&1 && echo Ok) > .nifireg &
-  ((curl -L http://$host:7788/                || curl -k -L https://$host:7790/)                2>/dev/null | egrep "<title>Schema Registry</title>|Error 401 Authentication required" > /dev/null 2>&1 && echo Ok) > .schreg &
-  ((curl -L http://$host:9991/                || curl -k -L https://$host:9991/)                2>/dev/null | grep "<title>STREAMS MESSAGING MANAGER</title>" > /dev/null 2>&1 && echo Ok) > .smm &
-  ((curl -L http://$host:8888/                ;  curl -k -L https://$host:8889/)                2>/dev/null | grep "<title>Hue" > /dev/null 2>&1 && echo Ok) > .hue &
-  ((curl -L http://cdsw.$ip.nip.io/           || curl -k -L https://cdsw.$ip.nip.io/)           2>/dev/null | egrep "<title.*(Cloudera Machine Learning|Cloudera Data Science Workbench)" > /dev/null 2>&1 && echo Ok) > .cml &
-  (token=$(curl -X POST --cookie-jar .cj --cookie .cj -H "Content-Type: application/json" --data '{"_local":false,"login":"admin","password":"supersecret1"}' "$CDSW_API/authenticate" 2>/dev/null | jq -r '.auth_token' 2> /dev/null) && \
-   curl -X POST --cookie-jar .cj --cookie .cj -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data '{"projectOwnerName":"admin","latestModelDeployment":true,"latestModelBuild":true}' "$CDSW_ALTUS_API/models/list-models" 2>/dev/null | jq -r '.[].latestModelDeployment | select(.model.name == "IoT Prediction Model").status' 2>/dev/null) > .model &
+  ("${CURL[@]}" http://$host/api/ping 2>/dev/null | grep 'Pong!' > /dev/null 2>&1 && echo Ok) > .web.$$ &
+  ("${CURL[@]}" http://$host:7180/cmf/login 2>/dev/null | grep "<title>Cloudera Manager</title>" > /dev/null 2>&1 && echo Ok) > .cm.$$ &
+  (("${CURL[@]}" http://$host:10080/efm/ui/        || "${CURL[@]}" https://$host:10080/efm/ui/)        2>/dev/null | grep "<title>CEM</title>" > /dev/null 2>&1 && echo Ok) > .cem.$$ &
+  (("${CURL[@]}" http://$host:8080/nifi/           || "${CURL[@]}" https://$host:8443/nifi/)           2>/dev/null | grep "<title>NiFi</title>" > /dev/null 2>&1 && echo Ok) > .nifi.$$ &
+  (("${CURL[@]}" http://$host:18080/nifi-registry/ || "${CURL[@]}" https://$host:18433/nifi-registry/) 2>/dev/null | grep "<title>NiFi Registry</title>" > /dev/null 2>&1 && echo Ok) > .nifireg.$$ &
+  (("${CURL[@]}" http://$host:7788/                || "${CURL[@]}" https://$host:7790/)                2>/dev/null | egrep "<title>Schema Registry</title>|Error 401 Authentication required" > /dev/null 2>&1 && echo Ok) > .schreg.$$ &
+  (("${CURL[@]}" http://$host:9991/                || "${CURL[@]}" https://$host:9991/)                2>/dev/null | grep "<title>STREAMS MESSAGING MANAGER</title>" > /dev/null 2>&1 && echo Ok) > .smm.$$ &
+  (("${CURL[@]}" http://$host:8888/                ;  "${CURL[@]}" https://$host:8889/)                2>/dev/null | grep "<title>Hue" > /dev/null 2>&1 && echo Ok) > .hue.$$ &
+  (("${CURL[@]}" http://cdsw.$ip.nip.io/           || "${CURL[@]}" https://cdsw.$ip.nip.io/)           2>/dev/null | egrep "<title.*(Cloudera Machine Learning|Cloudera Data Science Workbench)" > /dev/null 2>&1 && echo Ok) > .cml.$$ &
+  (token=$("${CURL[@]}" -X POST --cookie-jar .cj.$$ --cookie .cj.$$ -H "Content-Type: application/json" --data '{"_local":false,"login":"admin","password":"supersecret1"}' "$CDSW_API/authenticate" 2>/dev/null | jq -r '.auth_token' 2> /dev/null) && \
+   "${CURL[@]}" -X POST --cookie-jar .cj.$$ --cookie .cj.$$ -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data '{"projectOwnerName":"admin","latestModelDeployment":true,"latestModelBuild":true}' "$CDSW_ALTUS_API/models/list-models" 2>/dev/null | jq -r '.[].latestModelDeployment | select(.model.name == "IoT Prediction Model").status' 2>/dev/null) > .model.$$ &
   wait
-  printf "%-30s %-30s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %s\n" "$instance" "$ip" "$(cat .web)" "$(cat .cm)" "$(cat .cem)" "$(cat .nifi)" "$(cat .nifireg)" "$(cat .schreg)" "$(cat .smm)" "$(cat .hue)" "$(cat .cml)" "$(cat .model)"
+  printf "%-30s %-30s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %s\n" "$instance" "$ip" "$(cat .web.$$)" "$(cat .cm.$$)" "$(cat .cem.$$)" "$(cat .nifi.$$)" "$(cat .nifireg.$$)" "$(cat .schreg.$$)" "$(cat .smm.$$)" "$(cat .hue.$$)" "$(cat .cml.$$)" "$(cat .model.$$)"
 done | sort -t\[ -k1,1r -k2,2n
