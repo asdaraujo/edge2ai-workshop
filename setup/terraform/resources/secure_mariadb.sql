352dc33f73e676a1be21f5e6d25c294237fc5661
Andre Araujo <araujo@cloudera.com>
2021-03-16 11:51:26 +1100

    Fix Flask dependencies

--- setup/terraform/resources/secure_mariadb.sql
+++
@@ -1,5 +0,0 @@
-UPDATE mysql.user SET Password=PASSWORD('supersecret1') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
