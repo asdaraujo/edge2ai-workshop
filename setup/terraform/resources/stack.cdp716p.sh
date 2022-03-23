eb622ffe476bf46f3c577b90451fdfb9295239b1
Andre Araujo <araujo@cloudera.com>
2022-03-23 12:05:57 +1100

    Updated Maven repository URL to point to version 3.8.5

--- setup/terraform/resources/stack.cdp716p.sh
+++ setup/terraform/resources/stack.cdp716p.sh
@@ -17,7 +17,7 @@ REMOTE_REPO_PWD=<YOUR_PASSWORD_HERE>
 JAVA_PACKAGE_NAME=java-1.8.0-openjdk-devel
 
 ##### Maven binary
-MAVEN_BINARY_URL=https://downloads.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+MAVEN_BINARY_URL=https://downloads.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 
 #####  CM
 CM_VERSION=7.3.1
