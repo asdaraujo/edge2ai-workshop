cc603eeff7ea6298518bf20da4fef67f59a8c7d0
Andre Araujo <araujo@cloudera.com>
2022-04-06 06:27:50 +1000

    Bumped versions in public stack.
    CDSW 1.10.0
    CSA 1.6.2.0
    CFM 1.3.1.0

--- setup/terraform/resources/stack.cdp717p.sh
+++ setup/terraform/resources/stack.cdp717p.sh
@@ -20,27 +20,27 @@ JAVA_PACKAGE_NAME=java-1.8.0-openjdk-devel
 MAVEN_BINARY_URL=https://downloads.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 
 #####  CM
-CM_VERSION=7.4.4
+CM_VERSION=7.6.1
 CM_MAJOR_VERSION=${CM_VERSION%%.*}
-CM_REPO_AS_TARBALL_URL=https://archive.cloudera.com/p/cm7/7.4.4/repo-as-tarball/cm7.4.4-redhat7.tar.gz
+CM_REPO_AS_TARBALL_URL=https://archive.cloudera.com/p/cm7/7.6.1/repo-as-tarball/cm7.6.1-redhat7.tar.gz
 CM_BASE_URL=
 CM_REPO_FILE_URL=
 
 #####  CDH
 CDH_VERSION=7.1.7
-CDH_BUILD=7.1.7-1.cdh7.1.7.p0.15945976
+CDH_BUILD=7.1.7-1.cdh7.1.7.p1000.24102687
 CDH_MAJOR_VERSION=${CDH_VERSION%%.*}
-CDH_PARCEL_REPO=https://archive.cloudera.com/p/cdh7/7.1.7.0/parcels/
+CDH_PARCEL_REPO=https://archive.cloudera.com/p/cdh7/7.1.7.1000/parcels/
 
 #####  CFM
-CFM_VERSION=2.0.4.0
-CFM_BUILD=2.0.4.0-80
+CFM_VERSION=2.1.3.0
+CFM_BUILD=2.1.3.0-125
 CFM_MAJOR_VERSION=${CFM_VERSION%%.*}
-NIFI_VERSION=1.11.4
-NIFI_REGISTRY_VERSION=0.6.0
-CFM_PARCEL_REPO=https://archive.cloudera.com/p/CFM/2.x/redhat7/yum/tars/parcel/
-CFM_NIFI_CSD_URL=https://archive.cloudera.com/p/CFM/2.x/redhat7/yum/tars/parcel/NIFI-1.11.4.2.0.4.0-80.jar
-CFM_NIFIREG_CSD_URL=https://archive.cloudera.com/p/CFM/2.x/redhat7/yum/tars/parcel/NIFIREGISTRY-0.6.0.2.0.4.0-80.jar
+NIFI_VERSION=1.15.2
+NIFI_REGISTRY_VERSION=1.15.2
+CFM_PARCEL_REPO=https://archive.cloudera.com/p/cfm2/2.1.3.0/redhat7/yum/tars/parcel/
+CFM_NIFI_CSD_URL=https://archive.cloudera.com/p/cfm2/2.1.3.0/redhat7/yum/tars/parcel/NIFI-1.15.2.2.1.3.0-125.jar
+CFM_NIFIREG_CSD_URL=https://archive.cloudera.com/p/cfm2/2.1.3.0/redhat7/yum/tars/parcel/NIFIREGISTRY-1.15.2.2.1.3.0-125.jar
 
 #####  Anaconda
 ANACONDA_VERSION=2021.04
@@ -48,13 +48,13 @@ ANACONDA_PARCEL_REPO=https://repo.anaconda.com/pkgs/misc/parcels/
 
 #####  CDSW
 # If version is set, install will be attempted
-CDSW_VERSION=1.9.2
-CDSW_BUILD=1.9.2.p1.14556745
-CDSW_PARCEL_REPO=https://archive.cloudera.com/p/cdsw1/1.9.2/parcels/
-CDSW_CSD_URL=https://archive.cloudera.com/p/cdsw1/1.9.2/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDPDC-1.9.2.jar
+CDSW_VERSION=1.10.0
+CDSW_BUILD=1.10.0.p1.19362179
+CDSW_PARCEL_REPO=https://archive.cloudera.com/p/cdsw1/1.10.0/parcels/
+CDSW_CSD_URL=https://archive.cloudera.com/p/cdsw1/1.10.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDPDC-1.10.0.jar
 
 #####  CEM
-CEM_VERSION=1.2.1.0
+CEM_VERSION=1.2.2.0
 CEM_MAJOR_VERSION=${CEM_VERSION%%.*}
 EFM_VERSION=1.0.0
 MINIFI_VERSION=0.6.0
@@ -66,12 +66,12 @@ MINIFI_TARBALL_URL=https://archive.cloudera.com/p/CEM/centos7/1.x/updates/1.2.2.
 MINIFITK_TARBALL_URL=https://archive.cloudera.com/p/CEM/centos7/1.x/updates/1.2.2.0/tars/minifi/minifi-toolkit-0.6.0.1.2.2.0-14-bin.tar.gz
 
 #####   CSA
-CSA_VERSION=1.5.0.1
-FLINK_VERSION=1.13.2
-FLINK_BUILD=1.13.2-csa1.5.0.1-cdh7.1.7.0-551-17559653
-CSA_PARCEL_REPO=https://archive.cloudera.com/p/csa/1.5.0.1/parcels/
-FLINK_CSD_URL=https://archive.cloudera.com/p/csa/1.5.0.1/csd/FLINK-1.13.2-csa1.5.0.1-cdh7.1.7.0-551-17559653.jar
-SSB_CSD_URL=https://archive.cloudera.com/p/csa/1.5.0.1/csd/SQL_STREAM_BUILDER-1.13.2-csa1.5.0.1-cdh7.1.7.0-551-17559653.jar
+CSA_VERSION=1.6.2.0
+FLINK_VERSION=1.14.0
+FLINK_BUILD=1.14.0-csa1.6.2.0-cdh7.1.7.0-551-23013538
+CSA_PARCEL_REPO=https://archive.cloudera.com/p/csa/1.6.2.0/parcels/
+FLINK_CSD_URL=https://archive.cloudera.com/p/csa/1.6.2.0/csd/FLINK-1.14.0-csa1.6.2.0-cdh7.1.7.0-551-23013538.jar
+SSB_CSD_URL=https://archive.cloudera.com/p/csa/1.6.2.0/csd/SQL_STREAM_BUILDER-1.14.0-csa1.6.2.0-cdh7.1.7.0-551-23013538.jar
 
 # Parcels to be pre-downloaded during install.
 # Cloudera Manager will download any parcels that are not already downloaded previously.
