839f6becbdb4ed40916e6c4afa143ba9d6c03b1b
Andre Araujo <araujo@cloudera.com>
2022-08-03 08:54:17 +1000

    Added new SSB tests.

---
+++ setup/terraform/resources/tests/base/test_ssb.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Testing SSB
+"""
+import pytest
+import requests
+from ...labs import exception_context, retry_test
+from ...labs.workshop_nifi import CONSUMER_GROUP_ID, PRODUCER_CLIENT_ID
+from ...labs.utils import ssb, is_kerberos_enabled
+
+
+@pytest.mark.skipif(not ssb.is_csa16_or_later() or not ssb.is_ssb_installed() or not is_kerberos_enabled(),
+                    reason='CSA version is earlier than 1.6 or SSB is not deployed or Kerberos is not enabled')
+def test_ssb_upload_keytab():
+    resp = ssb.upload_keytab('admin', '/keytabs/admin.keytab')
+    assert resp.status_code == requests.codes.ok
+
+
+@pytest.mark.skipif(not ssb.is_csa17_or_later() or not ssb.is_ssb_installed() or not is_kerberos_enabled(),
+                    reason='CSA version is earlier than 1.7 or SSB is not deployed or Kerberos is not enabled')
+def test_ssb_generate_keytab():
+    resp = ssb.generate_keytab('admin', 'Supersecret1')
+    assert resp.status_code == requests.codes.ok
