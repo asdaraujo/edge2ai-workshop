63d721721c4f0c0c4a6f0f2975658fd6cc5ac5ea
Andre Araujo <araujo@cloudera.com>
2025-03-26 14:54:55 +1100

    Ensure that the public IP is a IPv4 address.

--- setup/terraform/lib/common.sh
+++ setup/terraform/lib/common.sh
@@ -432,7 +432,7 @@ function load_env() {
 function get_public_ip() {
   local retries=5
   while [[ $retries -gt 0 ]]; do
-    local public_ip=$(curl -sL http://ifconfig.me || curl -sL http://api.ipify.org/ || curl -sL https://ipinfo.io/ip)
+    local public_ip=$(curl -4sL http://ifconfig.me || curl -4sL http://api.ipify.org/ || curl -4sL https://ipinfo.io/ip)
     if [[ $public_ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
       echo $public_ip
       return
